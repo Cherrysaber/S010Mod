1e62c5932a1d92bebfb65f22e4d3546d914555b4
Cherrysaber <3082548039@qq.com>
2022-03-29 23:13:34 +0800

    ShopItemShow: 仙山洞府补丁移动到FuturePlugin

--- ShopItemShow/ShopItemShow.cs
+++ ShopItemShow/ShopItemShow.cs
@@ -278,46 +278,6 @@ namespace ShopItemShow
             }
             return true;
         }
-
-
-        // bugfix: 修复多人游戏洞府仙山可以无限升级和自动升级的bug
-        // 玩家升级仙山或者洞府后,设置 this.upgrade
-        // 但是在升完级后却没有重置 this.upgrade
-        // 导致地图上建造建筑物或者升级时
-        // 游戏都会遍历全部建筑调用 Upgrade
-
-        // 仙山,升级后重置 this.upgrade
-        [HarmonyPatch(typeof(ServerBuildingMountain), nameof(ServerBuildingMountain.Upgrade))]
-        [HarmonyPostfix]
-        public static void PatchMountainUpgrade(ServerBuildingMountain __instance)
-        {
-            if (__instance.upgrade != "")
-            {
-                Log.LogInfo("PatchMountainUpgrade: reset this.upgrade");
-                __instance.upgrade = "";
-                if (__instance.level == 2)
-                {
-                    __instance.goods[3].count = 0;
-                }
-            }
-
-        }
-
-        // 洞府,升级后重置 this.upgrade
-        [HarmonyPatch(typeof(ServerBuildingCave), nameof(ServerBuildingCave.Upgrade))]
-        [HarmonyPostfix]
-        public static void PatchCaveUpgrade(ServerBuildingCave __instance)
-        {
-            if (__instance.upgrade != "")
-            {
-                Log.LogInfo("PatchCaveUpgrade: reset this.upgrade");
-                __instance.upgrade = "";
-                if (__instance.level == 2)
-                {
-                    __instance.goods[3].count = 0;
-                }
-            }
-        }
     }
 
 
